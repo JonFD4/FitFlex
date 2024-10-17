9d6254c67be459d1bf92662250d128db91b18bce
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-10-17 20:33:51 +0000

    Work on filter functions

--- fitflexproduct/views.py
+++ fitflexproduct/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import WorkoutProgram as Product, WorkoutProgram, WorkoutCategory as Category
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q 
+from .models import WorkoutProgram as Product, WorkoutProgram, WorkoutCategory as Category, DifficultyLevel
 
 
 def all_products(request):
@@ -8,10 +10,32 @@ def all_products(request):
     """
     products= Product.objects.all()
     categories = Category.objects.all()
+    difficulty_levels = DifficultyLevel.objects.all()
+    query = None
+    selected_category = None
+   
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,"Enter a search criteria")
+                return redirect(reverse('products'))
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products= products.filter(queries)
 
+    # Category filter functionality
+        if 'category' in request.GET:
+                selected_category = request.GET('category').split(',')
+                if selected_category:  # Check if category is not None
+                    products = products.filter(category__friendly_name__iexact=selected_category)
+
+            
     context={
         'products':products,
         'categories': categories,
+        'difficulty_levels': difficulty_levels,
+        'search_term': query,
+        'selected_category': selected_category,
     }
     return render(request, 'products/products.html', context)
     
