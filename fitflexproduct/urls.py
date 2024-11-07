7cb16d28e8d7c7caa5f1ca216d46429a8b55adfb
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-11-07 22:14:55 +0000

    Add edit functionality

--- fitflexproduct/urls.py
+++ fitflexproduct/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/<int:product_id>/review/', views.submit_review, name='submit_review'), 
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
