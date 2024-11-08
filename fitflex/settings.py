edbed336368d949bfb92c1e23d3a52273de88ea1
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-11-08 23:36:29 +0000

    Adjust static file config

--- fitflex/settings.py
+++ fitflex/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5', 
     'FAQ',
     'user_profiles',
-    'storages'
+    'storages',
 
 
 ]
@@ -190,10 +190,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+    
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
